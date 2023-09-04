bca07e31eafa992e98149844c3d5b108a2f11706
Raquel-jp <lunablankaa25@hotmail.es>
2023-09-04 23:13:38 +0200

    primer paso

---
+++ calculadora/src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     <div className="contenedor-calculadora">
+      
+     </div>
+    </div>
+  );
+}
+
+export default App;

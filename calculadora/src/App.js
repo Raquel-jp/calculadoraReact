9985c5d7f5d132b4e8672394206845ca43215c60
Raquel-jp <lunablankaa25@hotmail.es>
2023-09-05 00:31:53 +0200

    commit 2

--- calculadora/src/App.js
+++ calculadora/src/App.js
@@ -1,13 +1,19 @@
 
 import './App.css';
+import Boton from './componentes/Boton';
 
 function App() {
   return (
     <div className="App">
      <div className="contenedor-calculadora">
-      <div></div>
-      <div></div>
-      <div></div>
+      <div className="fila">
+        <Boton>1</Boton>
+        <Boton>+</Boton>
+      </div>
+      <div className="fila"> </div>
+      <div className="fila"></div>
+      <div className="fila"></div>
+      <div className="fila"></div>
       
      </div>
     </div>

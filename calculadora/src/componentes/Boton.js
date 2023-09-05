c6d67197874b4b64df151e9bb780622af701ae24
Raquel-jp <lunablankaa25@hotmail.es>
2023-09-05 23:25:59 +0200

    parte final

--- calculadora/src/componentes/Boton.js
+++ calculadora/src/componentes/Boton.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import '../hojas-de-estilo/Boton.css';
 
 function Boton(props){
 
@@ -7,7 +8,8 @@ function Boton(props){
    };
 
     return(
-      <div className={`boton-contenedor ${esOperador(props.children) ? 'operador' : null}`}>
+      <div className={`boton-contenedor ${esOperador(props.children) ? 'operador' : null}`}
+      onClick={ ()=>props.manejarClic(props.children)}>
         {props.children}
       </div>
     );
